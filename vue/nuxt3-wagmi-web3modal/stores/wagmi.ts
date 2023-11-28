8f246c34dfca6861530f5b879d5824d9e51b64c8
hiephtdev <100410288+hiephtdev@users.noreply.github.com>
2023-11-28 16:41:39 +0700

    merge code fix error (#5)
    * add react folder
    
    * delete folder
    
    * add command deploy github page
    
    * Create nuxtjs.yml
    
    * Update nuxtjs.yml
    
    * Update nuxtjs.yml
    
    * Update nuxtjs.yml
    
    * Update nuxtjs.yml
    
    * Update nuxtjs.yml
    
    * Update nuxtjs.yml
    
    * Update contracts.ts
    
    * Update wagmi.ts

--- vue/nuxt3-wagmi-web3modal/stores/wagmi.ts
+++ vue/nuxt3-wagmi-web3modal/stores/wagmi.ts
@@ -11,10 +11,6 @@ import {
 export const defaultChain =
   import.meta.env.MODE === "development" ? lineaTestnet : linea;
 
-// You can name the return value of `defineStore()` anything you want,
-// but it's best to use the name of the store and surround it with `use`
-// and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
-// the first argument is a unique id of the store across your application
 export const useWagmi = defineStore("wagmi", () => {
   const projectId = useAppConfig().walletConnectProjectID;
   if (!projectId)
--- vue/nuxt3-wagmi-web3modal/utils/contracts.ts
+++ vue/nuxt3-wagmi-web3modal/utils/contracts.ts
@@ -1,6 +1,6 @@
 import { erc20ABI } from "@wagmi/core";
 
 export const usdcContractConfig = {
-  address: "0x6d8727B664D3f877de683F836E75EB2de47FD197", // zkSync Era Goerli Testnet DAI token address
+  address: "0x6d8727B664D3f877de683F836E75EB2de47FD197", // Linea Goerli Testnet USDC token address
   abi: erc20ABI,
 } as const;
